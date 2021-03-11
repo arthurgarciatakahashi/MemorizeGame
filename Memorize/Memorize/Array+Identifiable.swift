192a225b75384aca0d093a87aa54800dd26844e3
Arthur Garcia Takahashi <arthurgt@alumni.usp.br>
2021-03-10 22:17:30 -0300

    set card variable to an read only access control

---
+++ Memorize/Memorize/Array+Identifiable.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+extension Array where Element: Identifiable {
+    func firtIndex(matching: Element) -> Int? {
+        for index in 0..<self.count {
+            if self[index].id == matching.id {
+                return index
+            }
+        }
+        return nil
+    }
+}
