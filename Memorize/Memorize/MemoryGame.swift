b727878e00e4fb67a4d173522d2c3cac1d9db8e0
Arthur Garcia Takahashi <arthurgt@alumni.usp.br>
2021-03-18 03:00:19 -0300

    implements didst to face up and matched toggles

--- Memorize/Memorize/MemoryGame.swift
+++ Memorize/Memorize/MemoryGame.swift
@@ -44,8 +44,20 @@ struct MemoryGame<CardContent> where CardContent: Equatable{
 
     struct Card: Identifiable {
         var id: Int
-        var isFaceUp: Bool = false
-        var isMatched: Bool = false
+        var isFaceUp: Bool = false {
+            didSet {
+                if isFaceUp {
+                    startUsingBonusTime()
+                } else {
+                    stopUsingBonusTime()
+                }
+            }
+        }
+        var isMatched: Bool = false {
+            didSet {
+                stopUsingBonusTime()
+            }
+        }
         var content: CardContent
         
         
@@ -93,6 +105,5 @@ struct MemoryGame<CardContent> where CardContent: Equatable{
             pastFaceUpTime = faceUpTime
             self.lastFaceUpTime = nil
         }
-        
     }
 }
